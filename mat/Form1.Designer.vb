6fe7d3f2930f7c0ddeb50719c1a52dd1ac1551fb
ELHEIM19 <55957321+ELHEIM19@users.noreply.github.com>
2025-07-13 11:34:52 -0500

    subir proyecto

---
+++ mat/Form1.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        RichTextBox1 = New RichTextBox()
+        Button1 = New Button()
+        Label1 = New Label()
+        RichTextBox2 = New RichTextBox()
+        SuspendLayout()
+        ' 
+        ' RichTextBox1
+        ' 
+        RichTextBox1.Location = New Point(21, 52)
+        RichTextBox1.Name = "RichTextBox1"
+        RichTextBox1.Size = New Size(384, 200)
+        RichTextBox1.TabIndex = 1
+        RichTextBox1.Text = ""
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(429, 229)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(163, 29)
+        Button1.TabIndex = 2
+        Button1.Text = "Analizar"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(21, 52)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(0, 20)
+        Label1.TabIndex = 3
+        ' 
+        ' RichTextBox2
+        ' 
+        RichTextBox2.Enabled = False
+        RichTextBox2.Location = New Point(429, 49)
+        RichTextBox2.Name = "RichTextBox2"
+        RichTextBox2.Size = New Size(163, 174)
+        RichTextBox2.TabIndex = 4
+        RichTextBox2.Text = "Este programa le dirá si la matriz que ingrese es Antisimétrica o Única. Escriba aquí su matriz separando cada elemento por , y cada fila con ; como se muestra en el ejemplo"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(618, 278)
+        Controls.Add(RichTextBox2)
+        Controls.Add(Label1)
+        Controls.Add(Button1)
+        Controls.Add(RichTextBox1)
+        Name = "Form1"
+        Text = "Analisis de Matrices"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents RichTextBox2 As RichTextBox
+
+End Class
---
+++ mat/Form1.vb
@@ -0,0 +1,121 @@
+﻿Imports System.CodeDom.Compiler
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'iniciar richtexbox con una matriz de ejemplo'
+        RichTextBox1.Text = "0,0,0,1; " & Environment.NewLine &
+                            "0,0,1,0; " & Environment.NewLine &
+                            "0,1,0,0; " & Environment.NewLine &
+                            "1,0,0,0" 'matriz antisimetrica de ejemplo'
+
+        'iniciar richtexbox con una matriz de ejemplo que si es antisimetrica'
+        RichTextBox1.Text = "0,-1,-1; " & Environment.NewLine &
+                            "1,0,-4; " & Environment.NewLine &
+                            "1,4,0 " & Environment.NewLine
+        'matriz antisimetrica de ejemplo'
+
+    End Sub
+
+    Public Function EsAntisimetrica(matriz As Array) As String
+        Dim filas As Integer = matriz.GetLength(0)
+        Dim columnas As Integer = matriz.GetLength(1)
+        'verificar si la matriz es cuadrada'
+        If filas <> columnas Then
+            Return "No es cuadrada, no se puede verificar si es antisimetrica"
+        End If
+        'verificar si la matriz es antisimetrica'
+        For i As Integer = 0 To filas - 1
+            For j As Integer = 0 To columnas - 1
+                If i <> j AndAlso matriz.GetValue(i, j) <> -matriz.GetValue(j, i) Then
+                    Return "No es antisimetrica"
+                End If
+            Next
+        Next
+        Return "Es antisimetrica"
+    End Function
+
+    Public Function EsUnica(matriz As Array) As String
+        Dim filas As Integer = matriz.GetLength(0)
+        Dim columnas As Integer = matriz.GetLength(1)
+        'verificar si la matriz es cuadrada'
+        If filas <> columnas Then
+            Return "No es cuadrada, no se puede verificar si es unica"
+        End If
+        'verificar si la solo hay solo 0 y 1 en la matriz'
+        For i As Integer = 0 To filas - 1
+            Dim suma As Double = 0
+            For j As Integer = 0 To columnas - 1
+                Dim valor As Double = Convert.ToDouble(matriz.GetValue(i, j))
+                If valor <> 0 AndAlso valor <> 1 Then
+                    Return "No es unica"
+                End If
+                suma += valor
+
+            Next
+
+            'verificar si la suma de cada fila es igual a 1'
+            If suma <> 1 Then
+                Return "No es unica"
+            End If
+            suma = 0 'reiniciar la suma para la siguiente fila
+        Next
+        Return "Es unica"
+    End Function
+
+    Function convertirMatriz(texto As String) As Array
+        'separado  y por '
+        Dim filas As String() = Split(texto, ";")
+        Dim matriz As Array = Nothing
+        Dim tem As String()
+
+
+        For elemento As Integer = 0 To filas.Length - 1
+
+            tem = Split(filas(elemento), ",")
+
+            For i As Integer = 0 To tem.Length - 1
+
+                tem(i) = tem(i).Trim() 'eliminar espacios en blanco
+                ' MessageBox.Show(tem(i))
+                'rellenar la matriz con los valores convertidos a double'
+                If matriz Is Nothing Then
+                    matriz = New Double(filas.Length - 1, tem.Length - 1) {}
+                End If
+
+                matriz.SetValue(Convert.ToDouble(tem(i)), elemento, i)
+
+
+
+            Next
+
+            'MessageBox.Show(matriz.ToString)
+
+        Next
+
+        Return matriz
+
+    End Function
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        'capturar el texto del richtextbox'
+        Dim texto As String = RichTextBox1.Text
+        Dim matriz As Array = convertirMatriz(texto)
+
+        'llamar a las funciones para verificar si es antisimetrica o unica'
+        Dim resultadoAntisimetrica As String = EsAntisimetrica(matriz)
+        Dim resultadoUnica As String = EsUnica(matriz)
+
+        'mostrar los resultados en un messagebox'
+        MessageBox.Show("Resultado de antisimetrica: " & resultadoAntisimetrica & Environment.NewLine &
+                        "Resultado de unica: " & resultadoUnica, "Resultados")
+
+
+
+
+
+    End Sub
+
+
+End Class
